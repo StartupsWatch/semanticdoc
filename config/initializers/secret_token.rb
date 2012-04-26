e6f4d3e229413dd1b10dbb1167a1429642a43b1c
Mustafa TURAN <mustafaturan.net@gmail.com>
2012-04-26 14:38:35 +0300

    semanticdoc rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Semanticdoc::Application.config.secret_token = 'd9430f3ace0ff5eefddf03e6d180048fa9079bf1d8ff2ea247f0b87b041ca386ce6c9598644918332d6c3625ac92748e586a4d75db03afbaa53dc4d10cca4d8b'
